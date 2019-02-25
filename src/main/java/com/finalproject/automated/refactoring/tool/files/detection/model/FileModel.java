d18c2c14f7522f22b0fb2d2ec0d5955fe05f09d5
zapahlevie <zapahlevie@gmail.com>
2019-02-25 12:30:29 +0700

    Committed

--- src/main/java/com/finalproject/automated/refactoring/tool/files/detection/model/FileModel.java
+++ src/main/java/com/finalproject/automated/refactoring/tool/files/detection/model/FileModel.java
@@ -3,12 +3,6 @@ package com.finalproject.automated.refactoring.tool.files.detection.model;
 import lombok.Builder;
 import lombok.Data;
 
-/**
- * @author Faza Zulfika P P
- * @version 1.0.0
- * @since 1 October 2018
- */
-
 @Data
 @Builder
 public class FileModel {
